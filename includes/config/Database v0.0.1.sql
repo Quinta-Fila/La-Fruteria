4d83ec0814d0b0db2c6b0dfd0da7ae87b3875acf
Kike <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2024-03-07 18:22:32 +0100

    Añadidos productos e imagenes a la database y las sentencias sql para crear la databse

--- includes/config/Database v0.0.1.sql
+++ includes/config/Database v0.0.1.sql
@@ -18,6 +18,7 @@
 --
 -- Table structure for table `order_details`
 --
+
 CREATE DATABASE IF NOT EXISTS fruteria;
 
 USE fruteria;
@@ -32,7 +33,7 @@ CREATE TABLE `order_details` (
   `pedidos_id` int NOT NULL,
   KEY `fk_order_details_pedidos1_idx` (`pedidos_id`),
   KEY `fk_order_details_stock` (`stock_id`),
-  CONSTRAINT `fk_order_details_pedidos1` FOREIGN KEY (`pedidos_id`) REFERENCES `pedidos` (`id`),
+  CONSTRAINT `fk_order_details_pedidos1` FOREIGN KEY (`pedidos_id`) REFERENCES `pedidos` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_order_details_stock` FOREIGN KEY (`stock_id`) REFERENCES `stock` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -43,7 +44,7 @@ CREATE TABLE `order_details` (
 
 LOCK TABLES `order_details` WRITE;
 /*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
-INSERT INTO `order_details` VALUES (1,0.40,1,4),(1,0.40,1,5),(2,1.60,2,5),(2,0.60,3,5);
+INSERT INTO `order_details` VALUES (2,1.56,10,11),(1,1.30,11,11),(1,0.60,12,11),(1,2.00,13,11);
 /*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +64,7 @@ CREATE TABLE `pedidos` (
   PRIMARY KEY (`id`),
   KEY `fk_pedidos_usuarios1_idx` (`usuarios_id`),
   CONSTRAINT `fk_pedidos_usuarios1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,11 +73,7 @@ CREATE TABLE `pedidos` (
 
 LOCK TABLES `pedidos` WRITE;
 /*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
-INSERT INTO `pedidos` VALUES (1,6,'1',10.00,NULL),
-(2,1,'1',1.00,'2024-03-06'),
-(3,1,'1',1.00,'2024-03-06'),
-(4,1,NULL,0.40,'2024-03-06'),
-(5,1,NULL,2.60,'2024-03-06');
+INSERT INTO `pedidos` VALUES (11,8,NULL,5.46,'2024-03-07');
 /*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,7 +92,7 @@ CREATE TABLE `stock` (
   `categoria` varchar(45) NOT NULL,
   `imagen` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,7 +101,7 @@ CREATE TABLE `stock` (
 
 LOCK TABLES `stock` WRITE;
 /*!40000 ALTER TABLE `stock` DISABLE KEYS */;
-INSERT INTO `stock` VALUES (1,'Pera','100',0.40,'fruta',NULL),(2,'Lechuga','40',0.80,'verdura',NULL),(3,'Tomate','60',0.30,'fruta',NULL),(4,'Pepino','20',0.45,'verdura',NULL),(5,'Manzana','150',0.37,'fruta',NULL);
+INSERT INTO `stock` VALUES (9,'Kiwi','12',1.20,'Fruta','98801b4b03982dc30133e5a0fca5b497.jpg'),(10,'Manzana','30',0.78,'Fruta','b1ab62cda85cf4356bdaeda9e1d3b180.jpg'),(11,'Melon','50',1.30,'Fruta','09404fda2f226411327d86e67d224330.jpg'),(12,'Naranja','30',0.60,'Fruta','d5a92fa71717a7eef2f1116c5ce1d524.jpg'),(13,'Piña','10',2.00,'Fruta','e837146cb9d49e31146e2b57913f12a8.jpg'),(14,'Platanos','20',0.90,'Fruta','f65a86b3b3e9b0de273bf6aa4a7e8bc5.jpg'),(15,'Fresas','22',1.20,'Fruta','fd99daca21f79b6f319ee93f14c22e2c.jpg'),(16,'Lechuga','5',0.40,'Verdura','50f567a8d730fbe947f6c1da472a041c.jpg'),(17,'Aguacates','13',2.10,'Fruta','965e0957691024a7c91b8aaebdce22b5.jpg'),(18,'Arandanos','23',0.78,'Fruta','ad1d9039b3e6b5689af9781413c046a8.jpg');
 /*!40000 ALTER TABLE `stock` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +119,7 @@ CREATE TABLE `usuarios` (
   `password` varchar(255) NOT NULL,
   `rol` int NOT NULL DEFAULT '2',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,7 +128,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'a','a@ejemplo.com','12345',2),(5,'admin','admin@ejemplo.com','$2y$10$49Qz0oOS6taSSuwWyW8Ilup2ULFuiY0LxgJEcbs/jYhEEecusrLJe',1),(6,'usu3','usu3@ejemplo.com','$2y$10$Y5U6lKNFp7DBmJKaKtA29.DyDAKQJDFb8vMCqtMk/kAzG/r3siOeq',2);
+INSERT INTO `usuarios` VALUES (5,'admin','admin@ejemplo.com','$2y$10$49Qz0oOS6taSSuwWyW8Ilup2ULFuiY0LxgJEcbs/jYhEEecusrLJe',1),(6,'usu3','usu3@ejemplo.com','$2y$10$Y5U6lKNFp7DBmJKaKtA29.DyDAKQJDFb8vMCqtMk/kAzG/r3siOeq',2),(7,'usu4','usu4@gmail.com','$2y$10$EviQfWzicBlNJLjNxkdNNuHnGp5dOU1/jCkgGi1BTo0puHA35MpnK',2),(8,'Manuel Garcia','usuario1@gmail.com','$2y$10$F5.BRBJ3eL31aChQH9Cx6./kM6pMKGkdk8b6ZS3l4lK.4X5nExhyO',2);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -144,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-06 12:59:28
+-- Dump completed on 2024-03-07 18:19:22
